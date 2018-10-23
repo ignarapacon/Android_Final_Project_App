57d219270013fc65e408e62d92eb74c96f06f6c1
Igna Rapacon <2015081257@ust-ics.mygbiz.com>
2018-10-24 05:14:35 +0800

    Navigation Overlay Added (10/24/18)

--- app/src/main/java/com/rapacon/igna/android_final_project_app/MainActivity.java
+++ app/src/main/java/com/rapacon/igna/android_final_project_app/MainActivity.java
@@ -3,18 +3,25 @@ package com.rapacon.igna.android_final_project_app;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.os.Bundle;
 import android.view.MenuItem;
+
 
 public class MainActivity extends AppCompatActivity {
 
     private DrawerLayout mDrawerLayout;
     private ActionBarDrawerToggle mToggle;
 
+    private Toolbar mToolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mToolbar = (Toolbar) findViewById(R.id.nav_action);
+        setSupportActionBar(mToolbar);
 
         mDrawerLayout= (DrawerLayout) findViewById(R.id.drawerLayout);
         mToggle = new ActionBarDrawerToggle(this, mDrawerLayout, R.string.open, R.string.close);
